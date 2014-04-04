de989b42b9df89e02d8f2be133f07ee6ba4f52d3
Michael Bond <codemonkey85@gmail.com>
2014-04-04 11:46:16 -0400

    Added VS2010 project files

---
+++ PKMDS-Save-Editor/PKMDS-Save-Editor/frmMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PKMDS_Save_Editor
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
